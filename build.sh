2c267a31ca89b1fe1dccbcffa1d40afd140c2439
Juliano Mohr <juliaaano@gmail.com>
2021-04-27 10:47:47 +1000

    Fix image tag local

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 set -euxo pipefail
 
-mvn --show-version clean package -DskipTests -Dquarkus.container-image.build=true -Dquarkus.container-image.image=ghcr.io/juliaaano/quarkus:local
+mvn --show-version clean package -DskipTests -Dquarkus.container-image.build=true
 # mvn --show-version clean package -DskipTests -Pnative -Dquarkus.native.container-build=true -Dquarkus.container-image.build=true -Dquarkus.container-image.image=ghcr.io/juliaaano/quarkus-native:local
 
 echo "end of build.sh" > /dev/null
