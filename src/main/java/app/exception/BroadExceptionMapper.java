e5920c293121f59cedac01ca74021c8d52e7d58e
Juliano Mohr <juliaaano@gmail.com>
2021-06-21 12:33:48 +1000

    Several fixes updates

--- src/main/java/app/exception/BroadExceptionMapper.java
+++ src/main/java/app/exception/BroadExceptionMapper.java
@@ -1,18 +1,19 @@
-package app;
+package app.exception;
 
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 import static javax.ws.rs.core.Response.Status.INTERNAL_SERVER_ERROR;
 import java.util.ArrayList;
 import java.util.Map;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @Provider
-public class ExceptionMapper implements javax.ws.rs.ext.ExceptionMapper<Throwable> {
+public class BroadExceptionMapper implements ExceptionMapper<Throwable> {
 
-    private static final Logger log = LoggerFactory.getLogger(ExceptionMapper.class);
+    private static final Logger log = LoggerFactory.getLogger(BroadExceptionMapper.class);
 
     @Override
     public Response toResponse(final Throwable exception) {
