f15cdf05cd8d8ddb8f0f14c4ec4472411e54e539
Juliano Mohr <juliaaano@gmail.com>
2021-07-21 14:41:40 +1000

    Add NotFound exception mapper

--- src/main/java/app/exception/NotFoundExceptionMapper.java
+++ src/main/java/app/exception/NotFoundExceptionMapper.java
@@ -5,12 +5,17 @@ import javax.ws.rs.NotFoundException;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Provider
 public class NotFoundExceptionMapper implements ExceptionMapper<NotFoundException> {
 
+    private static final Logger log = LoggerFactory.getLogger(NotFoundExceptionMapper.class);
+
     @Override
     public Response toResponse(final NotFoundException exception) {
+        log.info("Exception mapped to NOT_FOUND response: {}", exception);
         return Response.status(NOT_FOUND).build();
     }
 }
